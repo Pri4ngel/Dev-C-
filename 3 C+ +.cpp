58eab30498774df457b528cde514473fc46497ee
Priscila Martins da Silva <prisca0295@hotmail.com>
2025-10-16 08:57:59 -0300

    Add files via upload

---
+++ 3 C+ +.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int a, b, c;
+	printf("digite tres numeros inteiros:");
+	scanf("%i" "%i" "%i", &a, &b, &c);
+	int soma = a + b + c;
+	printf("A soma e: %i", soma);
+	
+	return 0;
+}
